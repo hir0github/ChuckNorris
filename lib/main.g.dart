822f7b850d8053e7bfc5aef36d573eba206c7701
Almaz Dautov <adautov@MacBook-Air-Almaz.local>
2022-06-05 22:42:57 +0300

    chuck norries v1.0

---
+++ lib/main.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+JokeData _$JokeDataFromJson(Map<String, dynamic> json) => JokeData(
+      json['value'] as String,
+    );
+
+Map<String, dynamic> _$JokeDataToJson(JokeData instance) => <String, dynamic>{
+      'value': instance.joke,
+    };
